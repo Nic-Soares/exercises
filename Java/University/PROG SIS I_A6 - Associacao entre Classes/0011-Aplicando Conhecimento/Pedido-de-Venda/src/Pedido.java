eeb7ae7d314343570ebb034f79175842bf1291f1
Nicolas <nicolassoares.santos@outlook.com>
2022-09-17 18:20:19 -0300

    Resoluçao do Aplicando 06

--- Java/University/PROG SIS I_A6 - Associacao entre Classes/0011-Aplicando Conhecimento/Pedido-de-Venda/src/Pedido.java
+++ Java/University/PROG SIS I_A6 - Associacao entre Classes/0011-Aplicando Conhecimento/Pedido-de-Venda/src/Pedido.java
@@ -1,4 +1,21 @@
+import java.util.Scanner;
 public class Pedido {
   int numero;
+  Produto item;
+  int quantidade;
+  double valor;
 
+  Pedido (int numero, Produto item, int quantidade){
+    this.numero = numero;
+    this.item = item;
+    this.quantidade = quantidade;
+    this.valor = valor;
+  }
+  
+  //Armazena no atributo valor o resultado do preço do produto multiplicado pela quantidade vendida
+  public void calculaPedido() {valor = quantidade * item.preco;}
+
+  public void toString(double valor) {
+    System.out.println("Saldo insuficiente para saque");
+  }
 }
