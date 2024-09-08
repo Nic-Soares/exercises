51151d2e890051be8cbb79d7c560b5cf0096a64a
Nicolas Soares <nicolassoares.santos@outlook.com>
2024-09-08 11:01:26 -0300

    Refactor darts.py to calculate the score based on the distance from the center

--- exercism/python/darts/darts.py
+++ exercism/python/darts/darts.py
@@ -1,2 +1,14 @@
 def score(x, y):
-    pass
+    # Calcula a distânce do ponto (x, y) ao centro (0, 0)
+    distance = (x**2 + y**2) ** 0.5
+
+    # Verifica em qual região o dardo caiu e retorna a pontuação
+    if distance > 10:
+        return 0
+    elif distance > 5:
+        return 1
+    elif distance > 1:
+        return 5
+    return 10
+
+
