f3a1054da5242bb2b9fa8b818768574d1291864f
Nicolas Santos <nicolassoares.santos@outlook.com>
2026-08-14 00:42:19 +0000

    Add exemplo1.c/.exe with turma name and FCI founding year for Lab. 1 delivery

---
+++ university/cc/2026_2/alg_bool_circ_dig/class-1/exemplo1.c
@@ -0,0 +1,19 @@
+/*
+ * Lab. 1
+ * Exemplo 1
+ */
+
+#include <stdio.h>
+
+int main() {
+    // imprime algumas mensagens na tela
+    printf("Mackenzie - FCI\n");
+    printf("Algoritmos e ");
+    printf("Prog. II\n");
+    printf("Turma 201815117.000.02P11\n");
+    printf("Desde 1970\n");
+
+    // pausa antes de terminar o programa
+    system("pause");
+    return 0;
+}
